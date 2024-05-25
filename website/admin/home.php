0c227c9c48832b0483f4ae921556dff3d18999b6
Jhon.Arias <jhonarias91@gmail.com>
2024-05-25 09:10:34 -0500

    adding Jhon's on home.php

--- website/admin/home.php
+++ website/admin/home.php
@@ -8,7 +8,7 @@ $admin = Admins::current_admin();
 
 ?>
 
-<h2>Welcome to the awesome admin panel <?=h( $admin['login']) ?> </h2>
+<h2>Welcome to the Jhon's awesome admin panel <?=h( $admin['login']) ?> </h2>
 
 <a href="<?=h( Admins::$CREATE_URL )?>">Create a new user!</a>
 
